3bc74e155206c934531aa75d3630c4855df1fd3f
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2022-07-24 11:39:53 +0530

    modified the home page

---
+++ Home Page/day-5/index.js
@@ -0,0 +1,7 @@
+// document.querySelector("#sign").addEventListener("click",signIn);
+
+// function signIn(event){
+    // onclick="location.href='day-5\signIn.html';"
+    // window.open(day-5\signIn.html)
+    // window.location = day-5\signIn.html
+// }
